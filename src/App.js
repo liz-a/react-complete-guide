0723f17fa045b415008b8ff18a0a8ee6746432d6
“Liz <liz.ardolino@hotmail.com>
2020-03-02 17:40:23 +0000

    3.43

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
+import person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { age: 7 },
+      { age: 63 },
+      { age: 21 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState(
+      { persons: [{age: 100}, ...this.state.persons]}
+    );
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
+        <button onClick={this.switchNameHandler} >Switch Name</button>
+        {
+          this.state.persons.map(person => {
+            return <Person age={person.age}></Person>
+          })
+        }
       </div>
     );
   }
