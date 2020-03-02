dce75fc3c2c6e8c442396ed5d89481d7de24b4b1
“Liz <liz.ardolino@hotmail.com>
2020-03-02 17:50:59 +0000

    hooks

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 import person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       { age: 7 },
       { age: 63 },
       { age: 21 }
     ]
-  }
+  });
 
-  switchNameHandler = () => {
-    this.setState(
-      { persons: [{age: 100}, ...this.state.persons]}
+  const [otherState, setOtherState] = useState(101);
+
+  const switchNameHandler = () => {
+    setPersonsState(
+      { persons: [{age: 100}, ...personsState.persons]}
     );
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi I'm a React App</h1>
-        <button onClick={this.switchNameHandler} >Switch Name</button>
-        {
-          this.state.persons.map(person => {
-            return <Person age={person.age}></Person>
-          })
-        }
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hi I'm a React App</h1>
+      <button onClick={switchNameHandler} >Switch Name</button>
+      {
+        personsState.persons.map(person => {
+          return <Person age={person.age}></Person>
+        })
+      }
+    </div>
+  );
 }
 
-export default App;
+export default app;
